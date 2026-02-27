2b9c776571dd00dc4c8061ba3a8da46c7a13e35b
theLucasb <lucasrodrigodes@gmail.com>
2026-02-27 11:47:10 -0300

    Tipos de dados em JS

---
+++ inicio/dados3.js
@@ -0,0 +1,17 @@
+let idade = 24;
+console.log(typeof idade);
+console.log(typeof 24);
+
+let valor = 1500.50;
+console.log(typeof valor);
+
+let estaChovendo = false; // true
+console.log(typeof estaChovendo);
+
+let nome = "Lucas";
+console.log(typeof nome);
+
+console.log(1 + 1);
+console.log("1" + 1);
+
+console.log(valor * "valor")
---
+++ inicio/dados4.js
@@ -0,0 +1,6 @@
+const a = 3;
+
+b = a + 10;
+
+console.log(a);
+console.log(b);
