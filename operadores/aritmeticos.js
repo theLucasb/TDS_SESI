927e8908e1d08829b83f98b6e5e91539462199c6
theLucasb <lucasrodrigodes@gmail.com>
2026-03-05 12:12:54 -0300

    Operadores Aritméticos

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,17 @@
+// valores literais
+console.log(10 + 5); // adição
+console.log(10 - 5); // subtração
+console.log(10 * 5); // multiplicação
+console.log(10 / 5); // divisão
+console.log(10 % 5); // resto da divisão
+
+// valores variaveis
+
+let a = 100;
+let b = 50;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
