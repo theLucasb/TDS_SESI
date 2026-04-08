1f4fbb07d961aaf0f0fcca70db9456a2b11729ac
theLucasb <lucasrodrigodes@gmail.com>
2026-04-08 15:08:14 -0300

    Desafio 01

---
+++ atividades/desafio01.js
@@ -0,0 +1,6 @@
+let nomeProduto = "Monitor Gamer 144Hz";
+let precoUnitario = 1250.00;
+let emEstoque = true;
+
+// Output formatado com Template String
+console.log(`O item ${nomeProduto} custa R$ ${precoUnitario}. Disponibilidade: ${emEstoque}.`);
