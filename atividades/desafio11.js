3bf6fbca39ebff45956f44d3526cfafd51cf4c59
theLucasb <lucasrodrigodes@gmail.com>
2026-04-08 15:10:08 -0300

    Desafio 11

---
+++ atividades/desafio11.js
@@ -0,0 +1,7 @@
+let alunos = ["Ana", "Beto", "Carla", "Diego", "Elena", "Fabio", "Gisele", "Hugo", "Iris", "Lucas"];
+
+if (alunos.includes("Lucas")) {
+    console.log("Usuário encontrado no sistema");
+} else {
+    console.log("Usuário não localizado");
+}
