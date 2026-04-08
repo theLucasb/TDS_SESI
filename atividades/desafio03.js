cd241dc7956902815ba6fd559a8d8a0edd3410d4
theLucasb <lucasrodrigodes@gmail.com>
2026-04-08 15:08:33 -0300

    Desafio 03

---
+++ atividades/desafio03.js
@@ -0,0 +1,8 @@
+let idadeUsuario = 20;
+let possuiConvite = true;
+
+if (idadeUsuario > 18 && possuiConvite === true) {
+    console.log("Acesso autorizado ao evento VIP.");
+} else {
+    console.log("Acesso negado.");
+}
